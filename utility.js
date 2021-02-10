3503fd85961806a37eb9d76b43404f84574f72d6
unknown <#archgowda124@gmail.com>
2021-02-10 20:37:00 +0530

    [Archana]Added:Player Rolls the die to get number 1 to 6

--- utility.js
+++ utility.js
@@ -2,5 +2,12 @@ class Utility{
 
     startPosition = 0;
     endPosition = 100;
+    random = 0;
+
+    rollDie =()=> {
+        this.random = Math.floor((Math.random()*6)+1);
+        return this.random
+        
+    }
 }
 module.exports = new Utility()
